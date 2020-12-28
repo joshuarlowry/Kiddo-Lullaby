b4c8e1ea6997b824be4d91cc2d2b10e418d5f8b9
joshuarlowry <joshua.r.lowry@gmail.com>
2020-12-28 16:19:11 -0600

    removed pprint

--- app/src/server.py
+++ app/src/server.py
@@ -1,5 +1,4 @@
 import spotipy
-import pprint
 from spotipy.oauth2 import SpotifyOAuth
 
 scope = "user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative user-library-read"
