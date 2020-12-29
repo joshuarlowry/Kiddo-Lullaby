54bf20e1c890beb297562bb91fe38fad345f1b31
joshuarlowry <joshua.r.lowry@gmail.com>
2020-12-28 20:15:29 -0600

    server changes the song

--- app/src/server.py
+++ app/src/server.py
@@ -1,16 +1,39 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
+def play(contextUri):
+    print(("About to start playing {0} on device {1}.").format(contextUri,deviceID))
+    sp.start_playback(device_id=deviceID,context_uri=contextUri)
+
+def playSong(uris):
+    print(("About to start playing {0} on device {1}.").format(uris,deviceID))
+    sp.start_playback(device_id=deviceID,uris=uris)
+    sp.repeat('context',deviceID)
+
+def nowPlaying():
+    now = sp.current_playback()
+    print(("Playing on {0}").format(now['device']['name']))
+    track = now['item']['name']
+    artists = now['item']['album']['artists'][0]
+    print(("Playing {0} by {1}").format(track, artists['name']))
+
+def getActiveDevice():
+    devices = sp.devices()
+    activeDevice = devices['devices'][0]['id']
+    for idx, device in enumerate(devices['devices']):
+        print(("{0}, {1} - {2}").format(device['id'],device['name'],device['is_active']))
+        if(device['is_active']):
+            activeDevice = device['id']
+            print("Assigning active device.")
+    return activeDevice
+
+
+#Authentication
 scope = "user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative user-library-read"
-
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+deviceID = getActiveDevice()
+heyTiger = ['spotify:track:2WOM5LEDprdaJ6V6gnFK0Z']
+nowPlaying()
+playSong(heyTiger)
+nowPlaying()
 
-devices = sp.devices()
-for idx, device in enumerate(devices['devices']):
-    print(("{0}, {1} - {2}").format(device['id'],device['name'],device['is_active']))
-
-now = sp.current_playback()
-print(("Playing on {0}").format(now['device']['name']))
-track = now['item']['name']
-artists = now['item']['album']['artists'][0]
-print(("Playing {0} by {1}").format(track, artists['name']))
