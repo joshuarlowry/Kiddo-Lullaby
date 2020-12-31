202b39f124e08146cbbf8b37aa73a3d46907e3fd
joshuarlowry <joshua.r.lowry@gmail.com>
2020-12-30 21:41:27 -0600

    Added logic for device name display

--- app/src/server.py
+++ app/src/server.py
@@ -18,7 +18,7 @@ def nowPlaying(sp):
     #print(("Playing on {0}").format(now['device']['name']))
     track = now['item']['name']
     artists = now['item']['album']['artists'][0]
-    returnString = ("Playing {0} by {1}").format(track, artists['name'])
+    returnString = ("{0} by {1}").format(track, artists['name'])
     #print(returnString)
     return returnString
 
@@ -33,6 +33,15 @@ def getActiveDevice(sp):
             #print("Assigning active device.")
     return activeDevice
 
+def getActiveDeviceName(sp):
+    devices = sp.devices()
+    # note, idx is used to store the index of the returned tuple. 
+    for idx, device in enumerate(devices['devices']):
+        #print(("{0}, {1} - {2}").format(device['id'],device['name'],device['is_active']))
+        if(device['is_active']):
+            return device['name']
+    return ""
+
 def authenticationRoutine():
     scope = "user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-read-private playlist-read-collaborative user-library-read"
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
@@ -44,6 +53,7 @@ def index():
     sp, deviceID = authenticationRoutine()
     playingNow = sp.current_playback()
     devices = sp.devices()
+    activeDeviceName = getActiveDeviceName(sp)
     a = Airium()
     a('<!DOCTYPE html>')
     with a.html(lang="pl"):
@@ -55,14 +65,17 @@ def index():
             a.title(_t="Kiddo-Lullaby Home")
         with a.body():
             with a.h1(id="title", klass='title_header'):
-                a("Kiddo Lullaby")
+                if activeDeviceName == "":
+                    a(("Kiddo Lullaby"))
+                else:
+                    a(("Kiddo Lullaby on {0}").format(activeDeviceName))
             if playingNow is not None:
                 with a.div(klass="now-playing-card"):
                     with a.h3(id="id23409231", klass='main-header'):
                         a("Now Playing")
                     a.img(src=playingNow['item']['album']['images'][1]['url'], alt=nowPlaying(sp))
                     with a.p(id="idNowPlaying", klass='now-playing'):
-                        a(nowPlaying(sp))
+                        a(("Playing {0}").format(nowPlaying(sp)))
                     with a.div():
                         if playingNow['is_playing']:
                             with a.a(href="/pause", klass="w3-btn"):
